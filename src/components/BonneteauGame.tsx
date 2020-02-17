70aaaf7acea770611cd6851f2c1b03e46da72746
herisata <heryratovoarison@gmail.com>
2020-02-18 01:54:14 +0300

    Handle animations

--- src/components/BonneteauGame.tsx
+++ src/components/BonneteauGame.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import FlipMove from 'react-flip-move';
 import Cup from './Cup/Cup';
 import './styles.css';
 
@@ -6,13 +7,16 @@ interface Props {}
 
 const CUPS = [
   {
+    key: 0,
     hasBall: false,
   },
   {
+    key: 1,
+    hasBall: false,
+  },
+  {
+    key: 2,
     hasBall: true,
-  },
-  {
-    hasBall: false,
   },
 ];
 
@@ -22,35 +26,74 @@ enum GAME_STATE {
   WIN,
   LOSS,
 }
+const FLIP_DURATION = 1000;
+const CUP_MOVES = 7;
+
+const getRandomIndex = (max: number, except?: number) => {
+  let index: number | undefined;
+  while (!index || index === except) {
+    index = Math.floor(Math.random() * max + 1);
+  }
+  return index! - 1;
+};
 
 const BonneteauGame: React.FC<Props> = () => {
   // game state
   const [gameState, setGameState] = useState(GAME_STATE.IDLE);
+  const [cupPositions, setCupPositions] = useState(CUPS);
+  const [movesLeft, setMovesLeft] = useState(0);
 
   // handlers
   const onPlayButtonClick = () => {
     setGameState(GAME_STATE.STARTED);
+    setTimeout(() => {
+      setMovesLeft(CUP_MOVES);
+    }, FLIP_DURATION);
   };
   const onCupClick = (hasBall: boolean) => () => {
     if (gameState !== GAME_STATE.STARTED) return;
     setGameState(hasBall ? GAME_STATE.WIN : GAME_STATE.LOSS);
   };
 
+  const swapRandomCups = () => {
+    const pos1 = getRandomIndex(cupPositions.length);
+    const pos2 = getRandomIndex(cupPositions.length, pos1);
+    const positions = [...cupPositions];
+    // eslint-disable-next-line prefer-destructuring
+    positions[pos1] = positions.splice(pos2, 1, positions[pos1])[0];
+    setCupPositions(positions);
+    setTimeout(() => {
+      setMovesLeft(movesLeft - 1);
+    }, FLIP_DURATION);
+  };
+  useEffect(() => {
+    if (movesLeft) {
+      swapRandomCups();
+    }
+  }, [movesLeft]);
+
   return (
     <div className="parent">
       {gameState !== GAME_STATE.STARTED && gameState !== GAME_STATE.IDLE && (
-        <text className={gameState === GAME_STATE.WIN ? 'txt-win' : 'txt-loss'}>
+        <div className={gameState === GAME_STATE.WIN ? 'txt-win' : 'txt-loss'}>
           {gameState === GAME_STATE.WIN ? 'Gagné' : 'Perdu'}
-        </text>
+        </div>
       )}
-      <div className="main">
-        {CUPS.map(cup => (
-          <Cup
-            showBall={gameState !== GAME_STATE.STARTED && cup.hasBall}
-            onCupClick={onCupClick(cup.hasBall)}
-          />
+      <FlipMove
+        className="main"
+        duration={FLIP_DURATION}
+        easing="cubic-bezier(.12,.36,.14,1.2)"
+        // onStart={this.startMove.bind(this)}
+      >
+        {cupPositions.map(cup => (
+          <div key={cup.key} className="cup-container">
+            <Cup
+              showBall={gameState !== GAME_STATE.STARTED && cup.hasBall}
+              onCupClick={onCupClick(cup.hasBall)}
+            />
+          </div>
         ))}
-      </div>
+      </FlipMove>
       {gameState !== GAME_STATE.STARTED && (
         <button type="button" className="button" onClick={onPlayButtonClick}>
           Jouer
